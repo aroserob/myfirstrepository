c5b32b6ae06946358558989448016b115361b34e
Alex Rosero <aroserob@gmail.com>
2024-11-12 15:20:23 -0500

    nuevo archivo sh con el history

---
+++ archivo.sh
@@ -0,0 +1,194 @@
+    1  pwd
+    2  ls
+    3  ls
+    4  clear
+    5  pwd
+    6  pwd
+    7  ls
+    8  clear
+    9  ls
+   10  [200~
+   11  clear
+   12  [200~ssh-keygen -t ed25519 -C "your_email@example.com"
+   13  [200~ssh-keygen -t ed25519 -C "aroserob@gmail.com"
+   14  ssh-keygen -t rsa -b 4096 -C "aroserob@gmail.com"
+   15  ls .ssh
+   16  cat id_rsa.pub
+   17  cat .ssh/id_rsa.pub
+   18  clear
+   19  pwd
+   20  ls
+   21  mkdir mis_proyectos
+   22  ls
+   23  cd mis_proyectos
+   24  gitclone https://github.com/aroserob/myfirstrepository.git
+   25  git clone https://github.com/aroserob/myfirstrepository.git
+   26  ls
+   27  cd myfirstrepository
+   28  ls
+   29  cd /c
+   30  ls
+   31  cd Users/
+   32  ls
+   33  cd alex_
+   34  ls
+   35  pwd
+   36  cd desktop
+   37  ls
+   38  pwd
+   39  cd onedrive
+   40  ls
+   41  cd Desktop
+   42  ls
+   43  cd..
+   44  cd ..
+   45  ls
+   46  cd ..
+   47  ls
+   48  pwd
+   49  ls
+   50  cp /OneDrive/Desktop/MyFirstYaml-Param.txt ./
+   51  cp /OneDrive/Desktop/MyFirstYaml - Param.txt ./
+   52  cp /OneDrive/Desktop/MyFirstYaml-Param.txt ./
+   53  ls
+   54  cd OneDrive/Desktop
+   55  ls
+   56  cd ..
+   57  cd ..
+   58  pwd
+   59  ls
+   60  [A
+   61  cp /OneDrive/Desktop/MyFirsYaml-Param ./
+   62  ls
+   63  cd OneDrive
+   64  ls
+   65  ls
+   66  cat MyFirsYaml.txt
+   67  cat MyFirsYaml-Param
+   68  cat MyFirsYaml-Param.txt
+   69  cp MyFirsYaml-Param.txt ../../mis_proyectos
+   70  cd ..
+   71  cd ..
+   72  ls
+   73  cd mis_proyectos
+   74  ls
+   75  cd myfirstrepository
+   76  ls
+   77  ls
+   78  mv ../MyFirsYaml-Param.txt ./
+   79  ls
+   80  clear
+   81  git status
+   82  ls
+   83  git status
+   84  git add MyFirsYaml-Param.txt 
+   85  ls
+   86  git status
+   87  git commit MyFirsYaml-Param.txt
+   88  git config --global user.email "aroserob@gmail.com"
+   89  git commit MyFirsYaml-Param.txt
+   90  git commit -m "mi 1er commit"
+   91  git config --global user.email "aroserob@gmail.com"
+   92  git commit -m "mi 1er commit"
+   93  ls
+   94  ls
+   95  cd mis_proyectos
+   96  ls
+   97  cd myfirstrepository
+   98  ls
+   99  git commit -m "mi 1er commit"
+  100  git config --global user.email "aroserob@gmail.com"
+  101  git commit -m "mi 1er commit"
+  102  ls
+  103  git log
+  104  git push
+  105  git commit -m "mi 1er commit"
+  106  rm -f .git/index.lock
+  107  git commit -m "mi 1er commit"
+  108  git log
+  109  git push 
+  110  ls
+  111  cp ~/onedrive/desktop/MyfirsYaml.txt ./ 
+  112  ls
+  113  clear
+  114  pwd
+  115  ls
+  116  pwd
+  117  ls
+  118  gir status
+  119  git status
+  120  git add MyfirsYaml.txt 
+  121  ls
+  122  git status
+  123  clear
+  124  ls
+  125  git status
+  126  git commit -m "mi 2do commit"
+  127  git push
+  128  git log
+  129  git log
+  130  clear
+  131  log
+  132  git log
+  133  ls
+  134  ~/onedrive/desktop/ls
+  135  cp ~/onedrive/desktop/CloudFormationFile.txt 
+  136  cp ~/onedrive/desktop/CloudFormationFile.txt ./
+  137  ls
+  138  clear 
+  139  git status
+  140  git add CloudFormationFile.txt
+  141  ls
+  142  git status
+  143  git log
+  144  git push
+  145  git commit 
+  146  git commit "mi 3er commit"
+  147  git commit -m "mi 3er commit"
+  148  ls
+  149  pwd
+  150  pwd
+  151  ls
+  152  cd mis_proyectos
+  153  ls
+  154  cd myfirstrepository/
+  155  ls
+  156  git commit -m "mi 3er commit"
+  157  ls
+  158  git push
+  159  git log
+  160  clear
+  161  ls
+  162  git log
+  163  git status
+  164  git push
+  165  git log
+  166  git status
+  167  git add
+  168  git add MyFirsYaml
+  169  git add MyFirsYaml.txt
+  170  git push
+  171  git commit -m "mi commit test"
+  172  git add MyFirsYaml.txt
+  173  git commit -m "mi commit test"
+  174  git commit -am "mi commit test"
+  175  git commit -a "mi commit test"
+  176  git status
+  177  clear
+  178  git log
+  179  git status
+  180  git push
+  181  clear
+  182  git log
+  183  ls
+  184  git branch
+  185  git checkout feature-add-dh
+  186  git branch feature-add-dh
+  187  git checkout feature-add-dh
+  188  ls
+  189  pwd
+  190  ls
+  191  git status
+  192  touch archivo.sh
+  193  ls
+  194  history > archivo.sh
